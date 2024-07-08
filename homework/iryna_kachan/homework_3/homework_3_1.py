a99e7d5a49e853de7b66d03e050efd2b1da5d117
iryna-kachan <iryna.kachan89@gmail.com>
2024-07-08 16:20:39 +0300

    final version

--- homework/iryna_kachan/homework_3/homework_3_1.py
+++ homework/iryna_kachan/homework_3/homework_3_1.py
@@ -1,5 +1,5 @@
 a = int(input())
 b = int(input())
-print(a+b) #sum of two numbers
-print(a-b) #difference of two numbers
-print(a*b) #product of two numbers
+print(a+b) # sum of two numbers
+print(a-b) # difference of two numbers
+print(a*b) # product of two numbers
