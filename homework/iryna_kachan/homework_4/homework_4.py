68d9f6b4d13283d086c9d5d1d9da511900aa0a59
iryna-kachan <iryna.kachan89@gmail.com>
2024-07-11 13:27:11 +0300

    changed according recommendations

--- homework/iryna_kachan/homework_4/homework_4.py
+++ homework/iryna_kachan/homework_4/homework_4.py
@@ -6,7 +6,7 @@ my_dict['set'] = {1, 2, 3, 4, 5}
 print('last element of the tuple:', my_dict['tuple'][-1])  # last element of the tuple
 my_dict['list'][-1] = 'here'  # add new last element to list
 my_dict['list'].pop(1)  # deleting second element of the list
-my_dict['dict']['i am a tuple'] = 12345  # add new element to the dictionary
+my_dict['dict'][('i am a tuple',)] = 12345  # add new element to the dictionary
 my_dict['dict'].pop('age')  # deleting element of the dictionary
 my_dict['set'].add(6)  # add new element to the set
 my_dict['set'].pop()  # deleting element of the set
