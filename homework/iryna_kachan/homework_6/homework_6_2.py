228ff09ae05b5bcaa7bc2f8a23502c9cfd48f538
iryna-kachan <iryna.kachan89@gmail.com>
2024-07-15 12:00:24 +0300

    Fixed problems

--- homework/iryna_kachan/homework_6/homework_6_2.py
+++ homework/iryna_kachan/homework_6/homework_6_2.py
@@ -1,6 +1,6 @@
-user_input = int(input('yor number: '))
-for user_input in range(1,101):
-    if (user_input % 3 == 0) and  (user_input % 5 == 0):
+user_input = int(input('your number: '))
+for user_input in range(1, 101):
+    if (user_input % 3 == 0) and (user_input % 5 == 0):
         print('FuzzBuzz')
     elif user_input % 3 == 0:
         print('Fuzz')
