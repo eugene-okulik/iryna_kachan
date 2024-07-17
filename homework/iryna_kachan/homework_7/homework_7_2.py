dd7293aada0ece943dd635bbd04f00e1ec745a73
iryna-kachan <iryna.kachan89@gmail.com>
2024-07-17 09:57:27 +0300

    resolve with loop

--- homework/iryna_kachan/homework_7/homework_7_2.py
+++ homework/iryna_kachan/homework_7/homework_7_2.py
@@ -1,15 +1,3 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
-a = 'I'
-b = 'love'
-c = 'Python'
-d = '!'
-
-
-def calc(numb):
-    print(numb * words[numb])
-
-
-calc(a)
-calc(b)
-calc(c)
-calc(d)
+for key, value in words.items():
+    print(key * value)
